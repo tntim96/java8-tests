57caf3d1b6e740ef19edd7de861f5673fb89a454
tntim96 <tntim96@gmail.com>
2014-08-02 09:07:21 +1000

    Add basic test

---
+++ src/main/java/Nashorn.java
@@ -0,0 +1,10 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class Nashorn {
+    public static void main(String[] args) throws ScriptException {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval("print('Hello World!');");
+    }
+}
